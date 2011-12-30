ab5e402b009dfc055ef9da12e54adc86dfca34fe
Paul Everitt <paul@agendaless.com>
2011-12-30 11:06:51 -0500

    First shot

--- piledriver-lax/scratchpad.py
+++ piledriver-lax/scratchpad.py
@@ -1,30 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from models import Base
+from models import User
 
 engine = create_engine('sqlite:///:memory:')
 
-Base = declarative_base()
 
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
-
-    def __init__(self, name, fullname, password):
-        self.name = name
-
-        self.fullname = fullname
-        self.password = password
-
-    def __repr__(self):
-        return "<User('%s','%s', '%s')>" % (
-            self.name, self.fullname, self.password)
 
 Base.metadata.create_all(engine)
 
